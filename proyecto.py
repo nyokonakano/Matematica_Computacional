5f3605b9679b721893a66a7f6c1a90693cf56871
nyokonakano <mottatardio@gmail.com>
2025-10-10 17:45:26 -0500

    AÑADÍ UN METODO MENU PARA QUE EL USUSARIO ELIJA QUE METODOS DE CIFRADO USARÁ

--- proyecto.py
+++ proyecto.py
@@ -1,1 +1,12 @@
+import tkinter as tk
+from tkinter import ttk, messagebox, scrolledtext
+import math
 
+
+def menu_principal():
+    #menu
+   while True:
+        print("SISTEMA DE CRIPTOGRAFIA")
+
+if __name__ == "__main__":
+    menu_principal()
